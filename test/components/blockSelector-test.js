deaf9c226ed74ebad2f191ceb0cbf3f44beebb6a
Evan Sherwood <evan@neezer.net>
2015-01-27 16:02:25 -0800

    tsts working

---
+++ test/components/blockSelector-test.js
@@ -0,0 +1,8 @@
+var assert        = require('assert');
+var blockSelector = require('../../src/components/blockSelector.js');
+
+describe('a test', function() {
+  it('should be a result', function() {
+    assert(3, [1,2,3,4].indexOf(3));
+  });
+});
