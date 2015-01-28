c72d1551ab0905025832ee318d4eab613f155cc2
Evan Sherwood <evan@neezer.net>
2015-01-27 17:59:31 -0800

    valid tests

--- test/components/blockSelector-test.js
+++ test/components/blockSelector-test.js
@@ -1,8 +1,41 @@
-var assert        = require('assert');
+var assert        = require('better-assert');
 var blockSelector = require('../../src/components/blockSelector.js');
+var HeroBlock     = require('../../src/components/blocks/Hero.jsx');
+var ListBlock     = require('../../src/components/blocks/List.jsx');
+var BlockCreate   = require('../../src/components/blocks/Create.jsx');
 
-describe('a test', function() {
-  it('should be a result', function() {
-    assert(3, [1,2,3,4].indexOf(3));
+describe('blockSelector', function() {
+  describe('when given a string that starts with "hero"', function() {
+    it('returns an object with the hero block with the variant 001', function() {
+      var selector = blockSelector('hero-001');
+      assert(HeroBlock == selector.element);
+      assert('001' == selector.variant);
+    });
+
+    it('returns a different variant and the same block type', function() {
+      var selector = blockSelector('hero-643');
+      assert(HeroBlock == selector.element);
+      assert('643' == selector.variant);
+    });
+  });
+
+  describe('when given a string that starts with "list"', function() {
+    it('returns an object with the list block with the variant 423', function() {
+      var selector = blockSelector('list-423');
+      assert(ListBlock == selector.element);
+      assert('423' == selector.variant);
+    });
+
+    it('returns a different variant and the same block type', function() {
+      var selector = blockSelector('list-222');
+      assert(ListBlock == selector.element);
+      assert('222' == selector.variant);
+    });
+  });
+
+  it('returns a BlockCreate if no selector given', function() {
+    var selector = blockSelector();
+    assert(BlockCreate == selector.element);
+    assert(null == selector.variant);
   });
 });
